29d3ba5ad112281dc850b24e27caa3433174fcc3
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2022-06-07 16:05:24 -0300

    Add files via upload

---
+++ Third_semester/OOP/Rick_caseStudy/FourthVersion/functions/Builder.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FourthVersion.functions;
+
+/**
+ *
+ * @author Marcus Vinícius da Costa Graciano - BSI3
+ */
+public enum Builder {
+    FENDER, MARTIN, GIBSON, COLLINGS, OLSON, RYAN, PRS, ANY;
+    
+    @Override
+    public String toString(){
+        return switch (this) {
+            case FENDER -> "fender";
+            case MARTIN -> "martin";
+            case GIBSON -> "gibson";
+            case COLLINGS -> "collings";
+            case OLSON -> "olson";
+            case RYAN -> "ryan";
+            case PRS -> "prs";
+            case ANY -> "any";
+        };
+    }
+}
---
+++ Third_semester/OOP/Rick_caseStudy/FourthVersion/functions/InstrumentSpec.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FourthVersion.functions;
+
+/**
+ *
+ * @author Marcus Vinícius da Costa Graciano - BSI3
+ */
+public abstract class InstrumentSpec {
+    private Builder builder;
+    private String model;
+    private Type type;
+    private Wood backWood;
+    private Wood topWood;
+
+    public InstrumentSpec(Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+    }
+
+    public Builder getBuilder() {
+        return builder;
+    }
+
+    public String getModel() {
+        return model;    
+    }
+    
+    public Type getType() {
+        return type;
+    }
+
+    public Wood getBackWood() {
+        return backWood;
+    }
+
+    public Wood getTopWood() {
+        return topWood;
+    }
+
+    public boolean matches(InstrumentSpec otherSpec) {
+        if (builder != otherSpec.builder) {
+            return false;
+        }
+        if ((model != null) && (!model.equals("")) && (!model.equals(otherSpec.model))) {
+            return false;
+        }
+        if (type != otherSpec.type) {
+            return false;
+        }
+        if (backWood != otherSpec.backWood) {
+            return false;
+        }
+        if (topWood != otherSpec.topWood) {
+            return false;
+        }
+        return true;
+    }
+}
