8de2d407e48534fc8750728dfebfe83ef9fcc060
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2022-10-27 16:32:56 -0300

    Novos testes adicionados (e testados_

--- Fourth_semester/Data_structures_I/listasDuplamenteEncadeadas/main.c
+++ Fourth_semester/Data_structures_I/listasDuplamenteEncadeadas/main.c
@@ -1,21 +1,27 @@
 #include "listdup.h"
 
-int main(void)
-{
+int main(void) {
 	ListaDUPLA *l;
 	ListaDUPLA *achou;
 	l = inicializa();
-	
-	l = insere (l, 15);
-	l = insere (l, 20);
-	l = insere (l, 30);
-	l = insere (l, 40);
 
+	l = insere_ordenado(l, 10);
+	l = insere_ordenado(l, 15);
+	l = insere_ordenado(l, 20);
+	l = insere_ordenado(l, 30);
+	l = insere_ordenado(l, 40);
+	l = insere_ordenado(l, 50);
+
+	l = insere_ordenado(l, 5);
+	l = insere_ordenado(l, 1);
+	l = insere_ordenado(l, 3);
+	l = insere_ordenado(l, 0);
+
+	printf("Impressão do elementos da lista: \n");
 	imprime(l);
-  l = retira(l, 20);
-	
-	//imprime(l);
-   
-	
+	l = retira(l, 20);
+	printf("\nImpressão inversa dos elementos da lista: \n");
+	imprime_inverso(l);  
+
 	return 0;
 }
