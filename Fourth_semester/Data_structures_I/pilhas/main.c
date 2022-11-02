31ba07f45692e70fdfeeb26fdc2f25c37f90e9e2
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2022-11-02 15:39:07 -0300

    Update main.c

--- Fourth_semester/Data_structures_I/pilhas/main.c
+++ Fourth_semester/Data_structures_I/pilhas/main.c
@@ -1,8 +1,16 @@
 #include "pilha.h"
 
 int main(){
+    Pilha *p; // aponta para o topo da pilha
+    p = inicializa();
+    p = push(p, 10);
+    p = push(p, 30);
+    p = push(p, 40);
     
-    
+    imprime(p);
+    printf("Valor removido do topo: %d", pop(&p));
+    imprime(p);
+    libera(p);
     
     return 0;
 }
