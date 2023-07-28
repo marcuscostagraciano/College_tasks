7a2a82dace7704f8c6efbec51b5c728639b1381f
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-07-28 16:13:01 -0300

    passos do tutorial 6

--- Sixth_semester/dev_web_II/real-world-vue/src/router/main.js
+++ Sixth_semester/dev_web_II/real-world-vue/src/router/main.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from '@/views/AboutView.vue'
+import EventDetailsView from '@/views/EventDetailsView.vue'
 import EventListView from '@/views/EventListView.vue'
 
 const routes = [
-    { path: '/', name: 'event-list', component: EventListView },
     { path: '/about', name: 'about', component: AboutView },
+    { path: '/', name: 'event-list', component: EventListView },
+    { path: '/event/:id', name: 'event-details', props: true, component: EventDetailsView },
 ]
 
 const router = createRouter({
