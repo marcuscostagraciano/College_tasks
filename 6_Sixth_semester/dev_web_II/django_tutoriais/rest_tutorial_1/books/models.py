db3b9e96dd227a82c18b72e9700fe529be0b51f8
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-09-27 15:58:46 -0300

    style(books model): correct spacing and table naming

--- 6_Sixth_semester/dev_web_II/django_tutoriais/rest_tutorial_1/books/models.py
+++ 6_Sixth_semester/dev_web_II/django_tutoriais/rest_tutorial_1/books/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    subtitle = models.CharField(max_length=250)    
-    author = models.CharField(max_length=100)    
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
-    
+
+    class Meta:
+        db_table = 'books'
+
     def __str__(self) -> str:
         return f"{self.title}: {self.subtitle}"
