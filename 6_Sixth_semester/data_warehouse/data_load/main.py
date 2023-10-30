e6bbcb31bbdefc6910bc2ef809fc6914e0ad33cc
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-10-30 14:06:54 -0300

    solved problem with floats turning into ints

--- 6_Sixth_semester/data_warehouse/data_load/main.py
+++ 6_Sixth_semester/data_warehouse/data_load/main.py
@@ -1,15 +1,23 @@
 import numpy as np
 import pandas
 import mysql.connector
-from mysql.connector import errorcode
 
 
-df = pandas.read_csv('fact_sales.csv', sep=';')
+df = pandas.read_csv('x_factsales.csv', sep=';')
 
 # df = df.fillna("NULL")
 df = df.fillna(np.nan).replace([np.nan], [None])
-print(df.head())
 
+
+def replace_comma(data: str) -> str:
+    return data.replace(',', '.')
+
+
+# df['discountPercentage'] = df['discountPercentage'].apply(replace_comma)
+df['discounts'] = df['discounts'].apply(replace_comma)
+df['taxes'] = df['taxes'].apply(replace_comma)
+df['unitPrice'] = df['unitPrice'].apply(replace_comma)
+print(df.head())
 
 # df[df['dateKey'] == 0] = 1
 df['dateKey'] = df['dateKey'].replace(0, 1)
@@ -17,14 +25,11 @@ df['dateKey'] = df['dateKey'].replace(0, 1)
 
 list_df = df.values.tolist()
 
-# for _ in range(5):
-#     print(list_df[_])
+for _ in range(5):
+    print(list_df[_])
 
-try:
-    cnx = mysql.connector.connect(user='root', database='teste_dw')
-    cursor = cnx.cursor()
-except errorcode as er:
-    print(er)
+cnx = mysql.connector.connect(user='root', database='adventureworks_dw')
+cursor = cnx.cursor()
 
 query = ('INSERT INTO factsales(CurrencyKey,DateKey,ProductKey,SalesReasonKey,SpecialOfferKey,TerritoryKey,productAmount,discounts,taxes,salesAmount,unitPrice)'
          'VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)')
@@ -34,7 +39,7 @@ for item in list_df:
     print(item)
     cursor.execute(query, item)
     cnx.commit()
-    
+
 # cursor.executemany(query, list_df)
 # cnx.commit()
 
