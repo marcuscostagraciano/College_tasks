75d6c5f960db216d7707fb54786008b9e2c2b7b8
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-11-30 15:16:58 -0300

    implementation of class
    implemented the __init__ and add/remove product and the order value

--- 6_Sixth_semester/software_engineering_II/integration_testing/Pedido.py
+++ 6_Sixth_semester/software_engineering_II/integration_testing/Pedido.py
@@ -1,3 +1,36 @@
+from Produto import Produto
+from typing import TypeAlias
+
+Produtos: TypeAlias = Produto | list[Produto] | tuple[Produto]
+
+
 class Pedido:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, prod: None | Produtos = None) -> None:
+        self._produtos = []
+
+        match prod:
+            case Produto():
+                self._produtos = [prod]
+            case list() | tuple():
+                self._produtos.extend(prod)
+            case _:
+                raise TypeError("Only products are allowed")
+
+    def add_produto(self, produto: Produtos) -> None:
+        match produto:
+            case Produto():
+                self._produtos.append(produto)
+            case list() | tuple():
+                self._produtos.extend(produto)
+            case _:
+                raise TypeError("Only products are allowed")
+
+    def remove_produto(self, produto: Produto) -> None:
+        self._produtos.remove(produto)
+
+    @property
+    def valor_pedido(self) -> float | int:
+        return sum([prod.preco for prod in self._produtos])
+
+    def __str__(self) -> str:
+        return f"{[prod.nome_produto for prod in self._produtos]}"
