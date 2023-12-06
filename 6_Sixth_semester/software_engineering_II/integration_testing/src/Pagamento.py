a1b2e751f72e90a01193c1cb0714059666e391fe
Gianluca Starke <gianlucastarke@gmail.com>
2023-12-06 19:44:28 -0300

    estou doido

--- 6_Sixth_semester/software_engineering_II/integration_testing/src/Pagamento.py
+++ 6_Sixth_semester/software_engineering_II/integration_testing/src/Pagamento.py
@@ -1,18 +1,58 @@
+from .Autenticacao import Autenticacao
 from .Pedido import Pedido
+import pandas as pd
 import regex_spm
 
 
 class Pagamento:
-    def __init__(self, ped: Pedido) -> None:
+    def __init__(self, ped: Pedido, aut: Autenticacao) -> None:
         self._pedido = ped
         self._tipo_pagamento = None
+        self._conta = aut.conta
 
     def pagar(self) -> None:
-        print('Pago.')
+        try:
+            if(self.tipo_pagamento == None):
+                raise Exception('Escolha a forma de pagamento.')
+            
+            if(self.pedido.valor_pedido > self.saldo):
+                raise Exception('Não foi possível efetuar o pagamento: saldo insuficiente.')
+            
+            contas = pd.read_csv('mock_data/CONTAS.csv')
+
+            print(contas.loc[contas['conta' == self.conta], 'saldo'])
+
+            # if(self.tipo_pagamento == 'Pix'):
+            #     contas.loc[contas['conta' == self.conta], 'saldo'] -= self.pedido.valor_pedido
+        
+            # if(self.tipo_pagamento == 'Cartao'):
+            #     contas.loc[contas['conta' == self.conta], 'limite_disponivel'] -= self.pedido.valor_pedido
+
+            # contas.to_csv('mock_data/CONTAS.csv')
+
+        except Exception as e:
+            print(e)
+
+    @property
+    def saldo(self) -> float:
+        contas = pd.read_csv('mock_data/CONTAS.csv', index_col='conta')
+        conta = contas.loc[self.conta]
+        
+        if(self.tipo_pagamento == 'Pix'):
+            return conta.saldo
+        
+        if(self.tipo_pagamento == 'Cartao'):
+            return conta.limite_disponivel
+        
+        raise Exception('Escolha a forma de pagamento.')
 
     @property
     def pedido(self) -> str:
         return self._pedido
+    
+    @property
+    def conta(self) -> str:
+        return self._conta
 
     @property
     def tipo_pagamento(self) -> str:
@@ -36,5 +76,7 @@ class Pagamento:
         for produto in self.pedido.produtos:
             print(produto)
             produtos += f'{produto["codigo_produto"]} | {produto["nome_produto"]} | {produto["nome_fabricante"]} | {produto["preco"]}\n'
+
+        produtos += f'Valor Total: {self.pedido.valor_pedido}'
 
         return f'{produtos}'
--- 6_Sixth_semester/software_engineering_II/integration_testing/tests/Pagamento_Pedido.py
+++ 6_Sixth_semester/software_engineering_II/integration_testing/tests/Pagamento_Pedido.py
@@ -1,6 +1,7 @@
 from path_normalizer import normalize_path
 normalize_path()
 
+from src.Autenticacao import Autenticacao
 from src.Produto import Produto
 from src.Pedido import Pedido
 from src.Pagamento import Pagamento
@@ -10,6 +11,7 @@ def escolher_forma_pagamento(pagamento: Pagamento):
     forma_de_pagamento = input('Por favor, escolha uma forma de pagamento (Cartão/Pix): ')
     pagamento.tipo_pagamento = forma_de_pagamento
 
+aut = Autenticacao('123', 'jose@email.com')
 
 produtos = [
     Produto(67),
@@ -20,9 +22,12 @@ pedido = Pedido(produtos)
 print(pedido)
 print(pedido.produtos)
 
-pagamento = Pagamento(pedido)
+pagamento = Pagamento(pedido, aut)
 print(pagamento)
 
 print(pagamento.tipo_pagamento)
 escolher_forma_pagamento(pagamento)
 print(pagamento.tipo_pagamento)
+print(pagamento.conta)
+print(pagamento.saldo)
+pagamento.pagar()
