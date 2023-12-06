797cac28c7a9e087dd7d1969f3be121995730983
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-12-06 08:54:44 -0300

    style: more pythonic

--- 6_Sixth_semester/software_engineering_II/integration_testing/tests/Pagamento_Pedido.py
+++ 6_Sixth_semester/software_engineering_II/integration_testing/tests/Pagamento_Pedido.py
@@ -5,9 +5,11 @@ from src.Produto import Produto
 from src.Pedido import Pedido
 from src.Pagamento import Pagamento
 
+
 def escolher_forma_pagamento(pagamento: Pagamento):
     forma_de_pagamento = input('Por favor, escolha uma forma de pagamento (Cartão/Pix): ')
     pagamento.tipo_pagamento = forma_de_pagamento
+
 
 produtos = [
     Produto(67),
